aef1a6b3799034f2dc384412f1b6b39f19c9a9e6
Danielalves98 <daniel.xavier01@fatecitapetininga.edu.br>
2019-04-11 22:53:57 -0300

    Restaurantes

---
+++ meat-app-starter-master/src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,8 @@
+export interface Restaurant{
+  id: string
+  name: string
+  category: string
+  tempoEntrega: string
+  rating: number
+  imageRestaurant: string
+}
---
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
